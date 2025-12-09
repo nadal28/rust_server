b7dfcdf4600a857c31719a9fd66a8dc7008de331
nadal28 <adrianaragones1995@gmail.com>
2025-12-09 10:50:12 +0100

    add logfile parameter

--- rust_files/runds.sh
+++ rust_files/runds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
 
-./RustDedicated +server.identity "indestructible" -batchmode
+./RustDedicated +server.identity "indestructible" -batchmode -logfile "server.log"
+
