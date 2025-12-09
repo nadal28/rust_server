af6662d6efcb6f6befde0a025133334cc87a516a
nadal28 <adrianaragones1995@gmail.com>
2025-12-09 16:20:07 +0100

    tune gitignore

--- 2_install_oxide.sh
+++ 2_install_oxide.sh
@@ -2,5 +2,3 @@
 wget https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip
 unzip -o -d rust_files/ Oxide.Rust-linux.zip
 rm Oxide.Rust-linux.zip
-
-wget -P rust_files/oxide/plugins/ https://umod.org/plugins/BuildingActions.cs
