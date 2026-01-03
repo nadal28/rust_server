adad37d74375c5c586cfdcb458b355a65a83ceb9
nadal28 <adrianaragones1995@gmail.com>
2026-01-03 13:01:43 +0100

    header image

--- 1_install_rust.sh
+++ 1_install_rust.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 /home/rust/steamcmd/steamcmd.sh +force_install_dir /home/rust/rust_server/rust_files +login anonymous +app_update 258550 validate +quit
+# After the command above the file runds.sh is modified, so we need to reset it
+git checkout .
